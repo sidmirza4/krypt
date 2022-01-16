5ededbface9fc698e9fb09e628cc57c796cdc3d2
Mohd Shahid <sidmirza4@gmail.com>
2022-01-16 18:39:40 +0530

    change duplicate

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -67,7 +67,7 @@ const Welcome = () => {
 					<div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
 						<div className={`rounded-tl-2xl ${commonStyles}`}>Reliability</div>
 						<div className={commonStyles}>Secuirity</div>
-						<div className={`rounded-tr-2xl ${commonStyles}`}>Reliability</div>
+						<div className={`rounded-tr-2xl ${commonStyles}`}>Ethereum</div>
 						<div className={`rounded-bl-2xl ${commonStyles}`}>Web 3.0</div>
 						<div className={commonStyles}>Low fees</div>
 						<div className={`rounded-br-2xl ${commonStyles}`}>Blockchain</div>
